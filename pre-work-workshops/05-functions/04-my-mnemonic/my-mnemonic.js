9b6942ca6613f41029a335483bd765f7981880b6
manojrai200 <manojrai9801@gmail.com>
2025-07-08 17:10:57 +0545

    commit

--- pre-work-workshops/05-functions/04-my-mnemonic/my-mnemonic.js
+++ pre-work-workshops/05-functions/04-my-mnemonic/my-mnemonic.js
@@ -1,1 +1,16 @@
 // YOUR CODE BELOW
+function myMnemonic(string1, string2, string3, string4){
+    if(string4 !== undefined){
+        return string1[0] + string2[0] + string3[0] + string4[0];
+    }else if(string3 !== undefined){
+        return string1[0] + string2[0] + string3[0];
+    }else if(string2 !== undefined){
+        return string1[0] + string2[0];
+    }else{
+        return String(string1[0]);
+    }
+    
+
+}
+
+console.log(myMnemonic(''));
