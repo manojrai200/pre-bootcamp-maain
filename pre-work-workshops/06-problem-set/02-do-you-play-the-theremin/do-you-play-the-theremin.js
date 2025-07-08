ed573bdd728f99b99e565d337408ab60cd185013
manojrai200 <manojrai9801@gmail.com>
2025-07-08 17:26:03 +0545

    commit

--- pre-work-workshops/06-problem-set/02-do-you-play-the-theremin/do-you-play-the-theremin.js
+++ pre-work-workshops/06-problem-set/02-do-you-play-the-theremin/do-you-play-the-theremin.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+function doYouPlayTheTheremin(name){
+    if(name[0] === 's' || name[0] === 'S'){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+console.log(doYouPlayTheTheremin('mark'));
