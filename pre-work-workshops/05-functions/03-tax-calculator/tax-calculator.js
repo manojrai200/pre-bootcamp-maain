383a4e40aa5518e47eb79b2c33eb53aee5934624
manojrai200 <manojrai9801@gmail.com>
2025-07-08 16:35:44 +0545

    commit

--- pre-work-workshops/05-functions/03-tax-calculator/tax-calculator.js
+++ pre-work-workshops/05-functions/03-tax-calculator/tax-calculator.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+function taxCalculator(price, state){
+    if (state === 'NY'){
+        return price + .04 * price;
+    }else if (state === 'NJ'){
+        return price + .06625 * price;
+    }
+
+}
+
+console.log(taxCalculator(10, 'NJ'))
