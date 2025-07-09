563533c5cf6cbd82fd8530a830d4b2202e1e0be1
manojrai200 <manojrai9801@gmail.com>
2025-07-09 18:02:28 +0545

    commit

--- workshops/03-coercion-and-truthiness/03-my-or-my-and/my-or-my-and.js
+++ workshops/03-coercion-and-truthiness/03-my-or-my-and/my-or-my-and.js
@@ -1,1 +1,30 @@
+// ### My Or, My And
+
+// Define a function, `myOr`, that accepts three arguments of any type.
+
+// `myOr` should return the same result as the buit-in `||` operator:
+
+// ```javascript
+// myOr(true, true, false) === (true || true || false); // => true
+// ```
+
+// Then, define a function, `myAnd`, that accepts three arguments of any type.
+
+// `myAnd` should return the same result as the buit-in `&&` operator:
+
+// ```javascript
+// myAnd(1, 'apples', true) === (1 && 'apples' && true); // => true
+// ```
+
+
+
 // YOUR CODE BELOW
+
+function myOr(a, b, c){
+    return a || b || c;
+}
+
+function myAnd(a, b, c){
+    return a && b && c;
+}
+
