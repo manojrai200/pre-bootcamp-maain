50eecbea167e7135685ef5be49b29873e543fb97
manojrai200 <manojrai9801@gmail.com>
2025-07-09 20:05:45 +0545

    commit

--- workshops/04-scope/01-fullstack-hq/fullstack-hq.js
+++ workshops/04-scope/01-fullstack-hq/fullstack-hq.js
@@ -1,19 +1,36 @@
+// ### Fullstack HQ
+
+// Refactor the function `fullstackHQ` so that it returns the correct string:
+
+// ```javascript
+// fullstackHQ();
+// // => Fullstack HQ is at Planet Earth, United States, New York State, New York City
+// ```
+
+// The only things you should change are the parameter and/or variable names.
+
+
+
+
 // Edit the code below
 let place = 'Planet Earth';
 
 function fullstackHQ(){
+  debugger;
   return middleFunction('United States');
 
-  function middleFunction(place) {
+  function middleFunction(place1) {
     return innerFunction('New York State');
 
-    function innerFunction(place) {
-      place = 'New York City';
+    function innerFunction(place2) {
+      let place3 = 'New York City';
       return innermostFunction();
 
       function innermostFunction() {
-        return 'Fullstack HQ is at ' + place + ', ' + place + ', ' + place + ', ' + place;
+        return 'Fullstack HQ is at ' + place + ', ' + place1 + ', ' + place2 + ', ' + place3;
       }
     }
   }
 }
+
+console.log(fullstackHQ());
