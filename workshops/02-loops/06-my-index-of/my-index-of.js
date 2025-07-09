ee92d2d58dd95f40f0778d545247caeb118e6aac
manojrai200 <manojrai9801@gmail.com>
2025-07-09 16:40:12 +0545

    commit

--- workshops/02-loops/06-my-index-of/my-index-of.js
+++ workshops/02-loops/06-my-index-of/my-index-of.js
@@ -1,1 +1,22 @@
 // YOUR CODE BELOW
+function myIndexOf(source, searchValue, startIdx = 0){
+    let count = 0;
+    for (i = startIdx; i < source.length; i++){
+        if (source[i] === searchValue[0]){
+            startSearch = i;
+            for(j = 0; j < searchValue.length; j++){
+                if (source[startSearch] === searchValue[j]){
+                    startSearch++;
+                    count++;
+                }
+            }
+        
+        }
+        if (count === searchValue.length){
+            return i;
+        }
+    }
+    return -1;
+}
+
+console.log(myIndexOf('here and there', 'here', 4));
