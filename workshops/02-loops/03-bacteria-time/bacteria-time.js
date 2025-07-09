f65e6b5ba7723bac9363d2cdd79d636f36ec1514
manojrai200 <manojrai9801@gmail.com>
2025-07-09 12:38:49 +0545

    commit

--- workshops/02-loops/03-bacteria-time/bacteria-time.js
+++ workshops/02-loops/03-bacteria-time/bacteria-time.js
@@ -1,1 +1,15 @@
 // YOUR CODE BELOW
+function bacteriaTime(currentNum, targetNum){
+    debugger;
+    let counter = 0;
+    if (targetNum > currentNum){
+        for (let i = currentNum; i < targetNum; i *= 2){
+            counter++;
+        }
+    }else{
+        return console.log("targetNum must be larger than currentNum");
+    }
+    return counter * 20;
+}
+
+console.log(bacteriaTime(1, 8));
