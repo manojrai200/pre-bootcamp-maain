43097a4281713339a95c3ddb15f9a144a790a07a
manojrai200 <manojrai9801@gmail.com>
2025-07-09 13:12:31 +0545

    commit

--- workshops/02-loops/04-exponentiate/exponentiate.js
+++ workshops/02-loops/04-exponentiate/exponentiate.js
@@ -1,1 +1,15 @@
 // YOUR CODE BELOW
+function exponentiate(base, power){
+    debugger;
+    let result = 1;
+    if (power === 0){
+        return result;
+    }else {
+        for(i = 1; i <= power; i++){
+            result *= base;
+        }
+    }
+    return result; 
+}
+
+console.log(exponentiate(3, 8)); 
