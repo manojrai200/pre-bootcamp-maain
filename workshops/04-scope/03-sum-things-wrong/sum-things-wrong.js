bd4d07e254c94b38a5e36b1e087735ecf330454e
manojrai200 <manojrai9801@gmail.com>
2025-07-09 20:13:23 +0545

    commit

--- workshops/04-scope/03-sum-things-wrong/sum-things-wrong.js
+++ workshops/04-scope/03-sum-things-wrong/sum-things-wrong.js
@@ -1,8 +1,12 @@
+// ### Sum Things Wrong
+
+// The function `sumThingsWrong` is failing some of its tests. Why? Fix the code so
+// it works.
+
+
 let sum = 0;
 
 function sumThingsWrong(num1, num2) {
-  sum += num1;
-  sum += num2;
-
+  sum = num1 + num2;
   return sum;
 }
