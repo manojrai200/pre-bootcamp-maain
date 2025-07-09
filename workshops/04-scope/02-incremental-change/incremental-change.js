cd40b0ec0ad43364184e7889bd426693d1ee3c82
manojrai200 <manojrai9801@gmail.com>
2025-07-09 20:10:32 +0545

    commit

--- workshops/04-scope/02-incremental-change/incremental-change.js
+++ workshops/04-scope/02-incremental-change/incremental-change.js
@@ -1,3 +1,30 @@
-let counter = 0;
+// ### Incremental Change
+
+// Define a function `incrementalChange` that accepts a number and increments the global
+// variable counter by that amount. The function should also return the current
+// value of the counter after the increment operation.
+
+// Each successive call to incrementalChange should progressively change the value
+// held by the counter.
+
+// ```javascript
+// let counter = 0;
+
+// incrementalChange(2); // => 2
+// incrementalChange(6); // => 8
+// incrementalChange(-1); // => 7
+// ```
+
+
 
 // YOUR CODE BELOW
+let counter = 0;
+
+function incrementalChange(a){
+    counter += a;
+    return counter;
+}
+
+console.log(incrementalChange(2)); // => 2
+console.log(incrementalChange(6)); // => 8
+console.log(incrementalChange(-1)); // => 7
