54aa0b33c7b01c3410c3f759bc57845f47800757
manojrai200 <manojrai9801@gmail.com>
2025-07-09 11:36:19 +0545

    commit

--- workshops/02-loops/01-only-odds/only-odds.js
+++ workshops/02-loops/01-only-odds/only-odds.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+function onlyOdds(num){
+    sum = 0;
+    for(let i = num; i > 0; i--){
+        if(i % 2 !== 0){
+            sum += i;
+        }
+    }
+    return sum;
+}
+
+console.log(onlyOdds(10));
