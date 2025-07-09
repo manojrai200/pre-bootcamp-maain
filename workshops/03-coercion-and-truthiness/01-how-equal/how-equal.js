ee2257a99c2d90fa34d0e138e1f1fb0622ec35a7
manojrai200 <manojrai9801@gmail.com>
2025-07-09 17:57:43 +0545

    commit

--- workshops/03-coercion-and-truthiness/01-how-equal/how-equal.js
+++ workshops/03-coercion-and-truthiness/01-how-equal/how-equal.js
@@ -1,1 +1,31 @@
+// ### How Equal
+
+// Define a function, `howEqual`, that accepts two arguments of any type.
+
+// `howEqual` should return the string 'loosely' if the two values passed into
+// the function are loosely equal. `howEqual` should return 'strictly' if the two
+// values are strictly equal. `howEqual` should return the string 'not equal' if
+// the two values are neither strictly nor loosely equal.
+
+// ```javascript
+// howEqual(0, '0'); // => loosely
+// howEqual(3, 9/3); // => strictly
+// howEqual(true, 'truthy') // => not equal
+// ```
+
+
+
 // YOUR CODE BELOW
+
+function howEqual(a, b) {
+  if (a === b) {
+    return 'strictly';
+  } else if (a == b) {
+    return 'loosely';
+  } else {
+    return 'not equal';
+  }
+}
+
+
+console.log(howEqual(0 === -1, false));
--- workshops/03-coercion-and-truthiness/02-is-truthy/is-truthy.js
+++ workshops/03-coercion-and-truthiness/02-is-truthy/is-truthy.js
@@ -1,1 +1,44 @@
+
+// ### Is Truthy
+
+// Write a function `isTruthy` that accepts a single argument of any type.
+
+// `isTruthy` should return true if that argument is 'truthy'.
+
+// If the value is 'falsey', log out one of the messages below, corresponding to
+// the type of the value tested.
+
+// ```
+// 'The boolean value false is falsey'
+// 'The null value is falsey'
+// 'undefined is falsey'
+// 'The number 0 is falsey (the only falsey number)'
+// 'The empty string is falsey (the only falsey string)'
+// ```
+
+// ```javascript
+// isTruthy('I yearn for truth'); // => true
+// isTruthy(null); // => The null value is falsey
+// ```
+
+
 // YOUR CODE BELOW
+
+function isTruthy(a){
+    if(a){
+        return true;
+    }else if (a === false) {
+        return 'The boolean value false is falsey';
+    }else if (a === null){
+        return 'The null value is falsey';
+    }else if (a === undefined){
+        return 'undefined is falsey';
+    }else if (a === 0){
+        return 'The number 0 is falsey (the only falsey number)'
+    }else if (a === ''){
+        return 'The empty string is falsey (the only falsey string)'
+    }
+
+}
+
+console.log(isTruthy(0));
