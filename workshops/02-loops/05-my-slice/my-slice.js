e1f2408a9ec82525b28cb7092350e4054791b0d0
manojrai200 <manojrai9801@gmail.com>
2025-07-09 15:26:21 +0545

    commit

--- workshops/02-loops/05-my-slice/my-slice.js
+++ workshops/02-loops/05-my-slice/my-slice.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+function mySlice(originalString, startIdx = 0, endIdx = originalString.length){
+   // debugger;
+    let finalString = "";
+    while(endIdx  >  startIdx){
+        finalString += originalString[startIdx];
+        startIdx++;
+    }
+    return finalString;
+}
