beba221d59604072eff4000c2c647e30d7978396
manojrai200 <manojrai9801@gmail.com>
2025-07-09 12:11:17 +0545

    commit

--- workshops/02-loops/02-crazy-caps/crazy-caps.js
+++ workshops/02-loops/02-crazy-caps/crazy-caps.js
@@ -1,1 +1,16 @@
 // YOUR CODE BELOW
+function crazyCaps(string){
+    debugger;
+    let newString = '';
+    for(let i = 0; i < string.length; i++){
+        if (i % 2 == 0){
+            newString += string[i].toLowerCase();
+        }else{
+            newString += string[i].toUpperCase();
+        }
+    }
+
+    return newString;
+}
+
+console.log(crazyCaps("fullstack is amazing"));
