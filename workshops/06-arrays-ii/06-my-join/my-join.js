771ec7e5818148544beff742d582b02e58aa8a24
manojrai200 <manojrai9801@gmail.com>
2025-07-10 17:31:46 +0545

    commit

--- workshops/06-arrays-ii/06-my-join/my-join.js
+++ workshops/06-arrays-ii/06-my-join/my-join.js
@@ -1,1 +1,54 @@
-// YOUR CODE BELOW
+// ### My Join
+
+// Define a function, `myJoin`, that accepts up to two arguments:
+//   1. array
+//   2. separator (string, optional)
+
+// `myJoin` should return a string with all of the elements from the array joined
+// togther. The separator should separate the joined elements:
+
+// ```javascript
+// myJoin(['let\'s', 'make', 'a', 'list'], ' '); // => "let's make  a list"
+// myJoin(['a', 'b', 'c'], '+'); // => "a+b+c"
+// ```
+
+// If separator is undefined, use ',' as the default separator.
+
+// ```javascript
+// myJoin(['Peter', 'Paul', 'Mary']); // => "Peter,Paul,Mary"
+// ```
+
+// If any elements in the array are undefined or null, they should be replaced with
+// an empty string in the returned string.
+
+// ```javascript
+// myJoin(['hello', undefined, 'world'], '-'); // => "hello--world"
+// ```
+
+// Do not use the built-in [`.join` array method](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/joins) in your answer. Feel free to
+// use it in all future problems though!
+
+
+// // YOUR CODE BELOW
+
+function myJoin(array, separator = ','){
+    debugger;
+
+    let string = '';
+    if(array[0] !== null){
+        string = array[0];;
+    }
+    for(let i = 1; i < array.length; i++){
+        if(array[i] === null || undefined){
+            array[i] = '';
+            string += separator + array[i];
+        }else {
+            string += separator + array[i];
+        }
+    }
+
+    return string;
+
+}
+
+console.log(myJoin([null, 'came, ', 'saw, ', 'conquered.'], 'I '));
