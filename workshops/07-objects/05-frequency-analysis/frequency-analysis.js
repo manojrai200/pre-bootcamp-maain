b4283b9f33949072162d833d35510164417e8c0a
manojrai200 <manojrai9801@gmail.com>
2025-07-10 21:38:43 +0545

    commit

--- workshops/07-objects/05-frequency-analysis/frequency-analysis.js
+++ workshops/07-objects/05-frequency-analysis/frequency-analysis.js
@@ -1,1 +1,20 @@
 // YOUR CODE BELOW
+function frequencyAnalysis(string){
+    let obj = {}
+    string = string.toLowerCase();
+    for(let i = 0; i < string.length; i++){
+
+        if(string[i] === ' '){
+            continue;
+        }
+
+        if (obj[string[i]]){
+            obj[string[i]]++;
+        }else {
+            obj[string[i]] = 1;
+        }
+    }
+    return obj;
+}
+
+console.log(frequencyAnalysis('i like pumpkins'));
