20a74bc4480cab96d63bab27368fdf5b5aaadffb
manojrai200 <manojrai9801@gmail.com>
2025-07-10 18:52:00 +0545

    commit

--- workshops/07-objects/02-compare-objects/compare-objects.js
+++ workshops/07-objects/02-compare-objects/compare-objects.js
@@ -1,1 +1,16 @@
 // YOUR CODE BELOW
+function compareObjects(o1, o2) {
+  let keys1 = Object.keys(o1);
+  let keys2 = Object.keys(o2);
+
+  if (keys1.length !== keys2.length){
+    return false;}
+
+ for(let keys of keys1){
+    if(o1[keys] !== o2[keys]){
+        return false;
+    }
+ }
+
+  return true;
+}
