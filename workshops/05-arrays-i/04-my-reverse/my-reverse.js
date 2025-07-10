7809421fcb32e2ebf0031678cd9630ff33addf2d
manojrai200 <manojrai9801@gmail.com>
2025-07-10 11:48:08 +0545

    commit

--- workshops/05-arrays-i/04-my-reverse/my-reverse.js
+++ workshops/05-arrays-i/04-my-reverse/my-reverse.js
@@ -1,1 +1,23 @@
-// YOUR CODE BELOW
+// ### My Reverse
+
+// Define a function `myReverse` that accepts an array.
+
+// `myReverse` should return a new array with the elements in reverse order.
+
+// ```javascript
+// myReverse([1, 2, 3]);    // => [3, 2, 1]
+// ```
+
+// Do not use the built-in [.reverse method](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reverse) for this problem. You can use it on all
+// future problems though!
+
+
+// // YOUR CODE BELOW
+function myReverse(array){
+    const newArray = [];
+    for(i = array.length - 1; i >= 0; i--){
+        newArray.push(array[i]);
+    }
+
+    return newArray; 
+}
