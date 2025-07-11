748dd73b654b411b116260360b44ae48aadeb638
manojrai200 <manojrai9801@gmail.com>
2025-07-11 18:07:50 +0545

    commit

--- workshops/11-higher-order-functions-ii/03-partial/partial.js
+++ workshops/11-higher-order-functions-ii/03-partial/partial.js
@@ -1,1 +1,18 @@
 // YOUR CODE BELOW
+
+
+function add(a, b) {
+    return a + b;
+}
+
+function partial(callBack, a){
+
+    return function(b){
+
+        return add(a, b);
+    }
+}
+
+
+let sumFive = partial(add, 5);
+console.log(sumFive(10));
