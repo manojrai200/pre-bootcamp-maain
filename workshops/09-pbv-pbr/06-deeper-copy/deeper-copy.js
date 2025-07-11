d5afe09a0e8df7d3cc8e04470e0092f9c23207b1
manojrai200 <manojrai9801@gmail.com>
2025-07-11 13:10:38 +0545

    commit

--- workshops/09-pbv-pbr/06-deeper-copy/deeper-copy.js
+++ workshops/09-pbv-pbr/06-deeper-copy/deeper-copy.js
@@ -1,1 +1,24 @@
 // YOUR CODE BELOW
+function deeperCopy(array){
+    let copy = []
+    for(let i = 0; i < array.length; i++){
+        let element = array[i];
+
+        if(Array.isArray(element)){
+            let nestedArray = [];
+
+            for(let j = 0; j < element.length; j++){
+                let nestedElement = element[j];
+                nestedArray.push(nestedElement);
+            }
+
+            copy.push(nestedArray);
+        }else {
+            copy.push(element);
+        }
+    }
+
+    return copy;
+}
+
+console.log(deeperCopy([1, [2, 3]]));
