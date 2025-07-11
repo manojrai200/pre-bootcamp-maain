5b6c5cc149600ba7ac058823f83e2cdd5b7e0628
manojrai200 <manojrai9801@gmail.com>
2025-07-11 20:52:39 +0545

    commit

--- workshops/08-object-methods/02-say-your-name/say-your-name.js
+++ workshops/08-object-methods/02-say-your-name/say-your-name.js
@@ -1,1 +1,17 @@
 // YOUR CODE BELOW
+
+let me2 = {
+    'name': 'Manoj',
+    getGreeting: function(name){
+        return ('Hi ' + friend.name + ', my name is ' + me2.name + '.');
+    }
+};
+
+
+let friend = {
+        name: 'Jeff'
+    };
+
+console.log(me2.name); 
+console.log(friend.name);
+console.log(me2.getGreeting(friend));
