da45edf64b148bd4abebbb1930ab7dc4810caba9
manojrai200 <manojrai9801@gmail.com>
2025-07-11 11:13:09 +0545

    commit

--- workshops/09-pbv-pbr/01-very-odd/very-odd.js
+++ workshops/09-pbv-pbr/01-very-odd/very-odd.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+
+function veryOdd(array){
+    let oddNum = [];
+    for(let i = 0; i < array.length; i++){
+        if(array[i] % 2 !== 0){
+            oddNum.push(array[i]);
+        }
+    }
+    return oddNum;
+}
