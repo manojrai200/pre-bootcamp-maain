beeb07b793501bb32b5eb23517e1005d3032396c
manojrai200 <manojrai9801@gmail.com>
2025-07-11 12:31:49 +0545

    commit

--- workshops/09-pbv-pbr/03-clone-machine/clone-machine.js
+++ workshops/09-pbv-pbr/03-clone-machine/clone-machine.js
@@ -1,1 +1,20 @@
 // YOUR CODE BELOW
+
+let dolly = {
+  name: 'Dolly',
+  species: 'sheep',
+  offspring: []
+}
+
+function cloneMachine(obj){
+    debugger;
+    let clone = {};
+    clone.name = obj.name + 'Clone';
+    clone.species = obj.species;
+    clone.offspring = [];
+
+    obj.offspring.push(clone.name);
+    return clone;
+}
+
+console.log(cloneMachine(dolly));
