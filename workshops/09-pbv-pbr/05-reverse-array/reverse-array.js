56604ed24442f3d1e94c10ebd33be6ba2b584193
manojrai200 <manojrai9801@gmail.com>
2025-07-11 12:56:41 +0545

    commit

--- workshops/09-pbv-pbr/05-reverse-array/reverse-array.js
+++ workshops/09-pbv-pbr/05-reverse-array/reverse-array.js
@@ -1,1 +1,16 @@
 // YOUR CODE BELOW
+function reverseArray(array){
+    debugger;
+    let demoArray = [];
+    for(let i = array.length - 1; i >= 0; i--){
+        demoArray.push(array.shift());
+    }
+
+    for(let i = 0; i < demoArray.length; i++){
+        array.unshift(demoArray[i]);
+    }
+
+    return array;
+}
+
+console.log(reverseArray(['here', 'we', 'go']));
