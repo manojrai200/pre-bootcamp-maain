426a15ab7e2f2a64776b7f0dbf54bc3a5f511e95
manojrai200 <manojrai9801@gmail.com>
2025-07-11 22:02:55 +0545

    commit

--- workshops/08-object-methods/04-tic-tac-toe/tic-tac-toe.js
+++ workshops/08-object-methods/04-tic-tac-toe/tic-tac-toe.js
@@ -1,1 +1,24 @@
 // YOUR CODE BELOW
+
+let ticTacToe = {
+    board: [[null, null, null],[null, null, null],[null, null, null]],
+    move: function(char, row, column){
+        if(ticTacToe.board[row][column] === null){
+        ticTacToe.board[row][column] = char;
+        }
+        return ticTacToe.board;
+    },
+
+    clear: function(){
+        for(let i = 0; i < 3; i++){
+            for(let j = 0; j < 3; j++){
+                if(ticTacToe.board[i][j] !== null){
+                    ticTacToe.board[i][j] = null;
+                }
+
+            }
+        }
+        return ticTacToe.board;
+    }
+}
+
