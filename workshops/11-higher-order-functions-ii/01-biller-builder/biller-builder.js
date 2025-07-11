905acdf648bdd857d5bc3a084a01f6769bdc9f2e
manojrai200 <manojrai9801@gmail.com>
2025-07-11 17:56:00 +0545

    commit

--- workshops/11-higher-order-functions-ii/01-biller-builder/biller-builder.js
+++ workshops/11-higher-order-functions-ii/01-biller-builder/biller-builder.js
@@ -1,1 +1,21 @@
 // YOUR CODE BELOW
+
+
+function billerBuilder(stateName){
+    if(stateName === 'NY'){
+        function calculates(num){
+            return num * 1.03 * 1.04;
+        }
+
+        return calculates;
+    }else if (stateName === 'NJ'){
+        function calculates(num){
+            return num * 1.05 * 1.06625;
+        }
+
+        return calculates;
+    }
+}   
+
+let newYorkBiller = billerBuilder('NY');
+console.log(newYorkBiller(100));
--- workshops/11-higher-order-functions-ii/02-times-table/times-table.js
+++ workshops/11-higher-order-functions-ii/02-times-table/times-table.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+function timesTable(a){
+    function product(b){
+        return a * b;
+    }
+    return product;
+}
+
+let ninesTable = timesTable(9);
+console.log(ninesTable(8));  
