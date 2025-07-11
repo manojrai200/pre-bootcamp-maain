895f4b2e736b7f775d43e20a385b31739839d086
manojrai200 <manojrai9801@gmail.com>
2025-07-11 19:03:02 +0545

    commit

--- workshops/11-higher-order-functions-ii/06-get-dog-breeder/get-dog-breeder.js
+++ workshops/11-higher-order-functions-ii/06-get-dog-breeder/get-dog-breeder.js
@@ -1,1 +1,23 @@
 // YOUR CODE BELOW
+
+
+function getDogBreeder(defaultName, defaultAge) {
+
+  function dogBreeder(name=defaultName, age=defaultAge) {
+    if (typeof name === 'number') {
+      age = name;
+      name = defaultName;
+    }
+
+    return  {
+      name: name,
+      age: age
+    }
+  }
+
+  return dogBreeder;
+
+}
+
+let dogBreeder = getDogBreeder('Rufus', 5);
+console.log(dogBreeder());
