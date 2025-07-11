16cf87f926fb624c024a7f35e5689c3105cd1c21
manojrai200 <manojrai9801@gmail.com>
2025-07-11 13:54:16 +0545

    commit

--- workshops/10-higher-order-functions-i/03-whos-a-special/whos-a-special.js
+++ workshops/10-higher-order-functions-i/03-whos-a-special/whos-a-special.js
@@ -1,1 +1,24 @@
 // YOUR CODE BELOW
+
+    let specialPets = [{
+        name: 'Mr. Binns',
+        species: 'cat'
+      }, {
+        name: 'Pusheen',
+        species: 'cat'
+      }
+    ];
+
+function whosASpecial(arrayOfPets){
+    let returnString = '';
+    for(let i = 0; i < arrayOfPets.length; i++){
+        if(i === 0){
+            returnString += arrayOfPets[i].name + ' the ' + arrayOfPets[i].species + ' is very special!';
+        }else {
+            returnString += ' ' + arrayOfPets[i].name + ' the ' + arrayOfPets[i].species + ' is very special!'
+        }
+    }
+    return returnString;
+}
+
+console.log(whosASpecial(specialPets));
--- workshops/10-higher-order-functions-i/04-my-for-each/my-for-each.js
+++ workshops/10-higher-order-functions-i/04-my-for-each/my-for-each.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+function myForEach(array, callBackFunc){
+
+    if(callBackFunc == undefined){
+        return undefined;
+    }
+
+    for(let i = 0; i < array.length; i++){
+        callBackFunc(array[i], i);
+    }
+}
