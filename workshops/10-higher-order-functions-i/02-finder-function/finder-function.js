6a88b7653f13adb934a5b487d01c0be876d4a4c6
manojrai200 <manojrai9801@gmail.com>
2025-07-11 13:32:11 +0545

    commit

--- workshops/10-higher-order-functions-i/02-finder-function/finder-function.js
+++ workshops/10-higher-order-functions-i/02-finder-function/finder-function.js
@@ -1,1 +1,21 @@
 // YOUR CODE BELOW
+    // function isApple(string) {
+    //   return string === 'apple';
+    // }
+
+    // let fruits = ['not an apple', 'not an apple', 'apple', 'not an apple'];
+
+
+
+function finderFunction(array, func){
+    debugger;
+    for(let i = 0; i < array.length; i++){
+        if(func(array[i])){
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+// finderFunction(fruits, isApple);
