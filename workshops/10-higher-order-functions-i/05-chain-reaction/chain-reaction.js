deb4ec7d766482f0f94e5d43cbd420440584e939
manojrai200 <manojrai9801@gmail.com>
2025-07-11 14:10:36 +0545

    commit

--- workshops/10-higher-order-functions-i/05-chain-reaction/chain-reaction.js
+++ workshops/10-higher-order-functions-i/05-chain-reaction/chain-reaction.js
@@ -1,1 +1,30 @@
 // YOUR CODE BELOW
+
+  function addTen(num) {
+    return num + 10;
+  }
+
+  function subtractFive(num) {
+    return num - 5;
+  }
+
+  function multiplyFive(num) {
+    return num * 5;
+  }
+
+function chainReaction(num, arrOfFunc){
+
+    debugger;
+    let newNum;
+    for(let i = 0; i < arrOfFunc.length; i++){
+        if(i === 0){
+        newNum = arrOfFunc[i](num);
+        }else {
+            newNum = arrOfFunc[i](newNum);
+        }
+    }
+
+    return newNum;
+}
+
+console.log(chainReaction(10, [addTen, multiplyFive, subtractFive]));
