6d8fc227bb7f3f056ef313994b53b96e860090f1
manojrai200 <manojrai9801@gmail.com>
2025-07-11 20:29:55 +0545

    commit

--- workshops/11-higher-order-functions-ii/07-cache-savings/cache-savings.js
+++ workshops/11-higher-order-functions-ii/07-cache-savings/cache-savings.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+function cacheSavings(callBackFunc){
+    let cache = {};
+    
+    return function (arg){
+        if(cache[arg] === undefined){
+            let callBackResult = callBackFunc(arg);
+            cache[arg] = callBackResult;
+            
+        }
+
+        return cache[arg];
+    }
+}
