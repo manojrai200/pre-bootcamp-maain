09195732a9e621a7eebcad6caa99c0b50d5cceb4
manojrai200 <manojrai9801@gmail.com>
2025-07-11 21:14:40 +0545

    commit

--- workshops/08-object-methods/03-call-them-all/call-them-all.js
+++ workshops/08-object-methods/03-call-them-all/call-them-all.js
@@ -1,1 +1,27 @@
 // YOUR CODE BELOW
+function callThemAll(obj, arg){
+    debugger;
+    let calledArray = []
+    for(let key in obj){
+        if(typeof obj[key] === 'function'){
+            calledArray.push(obj[key](arg));
+        }
+    }
+    return calledArray;
+}
+
+let numFun = {
+    addTwenty: function(num) {
+    return num + 20;
+    },
+
+    subtractTen: function(num) {
+    return num - 10;
+    },
+
+    stringify: function(num) {
+    return String(num);
+    }
+};
+
+console.log(callThemAll(numFun, 10));
